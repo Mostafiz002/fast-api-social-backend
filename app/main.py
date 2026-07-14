ecbe6ffd12f6467e285e7acb5b7493c85604adbd
Mostafiz002 <mo.mahin4@gmail.com>
2026-07-14 12:26:11 +0600

    feat: finished rew sql crud with psycopg

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 import time
-from typing import Optional
-from random import randrange
-from urllib import response
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
 import psycopg
 from psycopg.rows import dict_row 
 from pydantic import BaseModel 
@@ -60,16 +56,8 @@ def root():
 def get_posts(response: Response):
     cursor.execute("SELECT * FROM posts")
     posts = cursor.fetchall()
-    print(posts)
+    
     return {"data" : posts}
-
-@app.post("/posts", status_code=201)
-def create_post(post: Post):
-    cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
-
-    return {"data": new_post}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
@@ -81,12 +69,33 @@ def get_post(id: int):
     
     return {"post": post}
 
+@app.post("/posts", status_code=201)
+def create_post(post: Post):
+    cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+
+    return {"data": new_post}
+
 @app.delete("/posts/{id}", status_code=204)
 def delete_post(id: int):
     cursor.execute("DELETE FROM posts WHERE id=%s RETURNING *", (str(id),))
     deleted_post = cursor.fetchone()
+    conn.commit()
 
     if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     
     return Response(status_code=204)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *", (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    print(update_post)
+
+    if not updated_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    
+    return {"data": updated_post}
